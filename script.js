f1cf69965c974e8262e737709580686ce09dfcee
Kgl-01 <karthikgowda.mailme@gmail.com>
2023-08-30 21:15:38 +0530

    modal w/o styles

---
+++ script.js
@@ -0,0 +1,23 @@
+//selecting all the html elements using the querySelector
+const modal = document.querySelector("#modal");
+const openModalBtn = document.querySelector("#open-modal-btn");
+const closeModalBtn = document.querySelector("#close-modal-btn");
+const overlay = document.querySelector("#overlay");
+
+//adding an click eventListener to the open button to add open class
+openModalBtn.addEventListener("click", () => {
+  modal.classList.add("open");
+  overlay.classList.add("open");
+});
+
+//adding an click eventListener to the close button to remove open class
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
+});
+
+//adding an click eventListener to the overlay to remove open class
+overlay.addEventListener("click", () => {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
+});
